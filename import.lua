453c6db94e8046c73babd7409736045549fa9437
rifleh700 <rifleh700@protonmail.ch>
2021-01-27 23:58:33 +0400

    speedup

--- import.lua
+++ import.lua
@@ -1,11 +1,29 @@
 
+local resources = {}
+
+local function onStart(resource)
+	resources[getResourceName(resource)] = resource
+end
+addEventHandler("onResourceStart", root, onStart)
+addEventHandler("onClientResourceStart", root, onStart)
+
+local function onStop(resource)
+	resources[getResourceName(resource)] = nil
+end
+addEventHandler("onResourceStop", root, onStop)
+addEventHandler("onClientResourceStop", root, onStop)
+
 function import(resourceName, defineSpace, spaceName)
 	if type(resourceName) ~= "string" then error("bad argument #1 to 'import' (string expected)", 2) end
 	if defineSpace and defineSpace ~= true then error("bad argument #2 to 'import' (boolean expected)", 2) end
 	if spaceName and type("spaceName") ~= "string" then error("bad argument #3 to 'import' (string expected)", 2) end
 
 	local importResource = getResourceFromName(resourceName)
-	if not importResource then error(string.format("resource '%s' not found", resourceName), 2) end
+	if not importResource then error("resource '"..resourceName.."' not found", 2) end
+	if getResourceState(importResource) ~= "running" then error("resource '"..resourceName.."' is not running", 2) end
+	if importResource == getThisResource() then error("can't import from same resource", 2) end
+
+	resources[resourceName] = importResource
 
 	spaceName = defineSpace and (spaceName or resourceName)
 
@@ -17,11 +35,9 @@ function import(resourceName, defineSpace, spaceName)
 
 	for _, functionName in ipairs(getResourceExportedFunctions(importResource)) do
 		space[functionName] = function(...)
-			if not (importResource and getUserdataType(importResource) == "resource-data") then
-				importResource = getResourceFromName(resourceName)
-				if not importResource then error(string.format("resource '%s' not found", resourceName), 2) end
-			end
-			return call(importResource, functionName, ...)
+			local thisResource = resources[resourceName]
+			if not thisResource then error("resource '"..resourceName.."' is not running", 2) end
+			return call(thisResource, functionName, ...)
 		end
 	end
 
